024784a4d65d29a92a35951033d7c36f41bb77d2
musiakam <kamil.musial@tietoevry.com>
2023-03-25 10:07:08 +0100

    1

--- 1_regresja_liniowa.py
+++ 1_regresja_liniowa.py
@@ -1,4 +1,6 @@
 import pandas as pd    #'as'  alias
+import seaborn as sns
+import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
 df = pd.read_csv('weight-height.csv')
@@ -9,4 +11,14 @@ print(df.head(5))  #5 wierszy
 print(df.Gender.value_counts())
 df.Height *= 2.54
 df.Weight /= 2.2
+print(df.head(5))
+# gender, height - zmiennie niezależne -> weight - zmienna zależna
+# sns.displot(df.Weight)  # kobiety i mężczyźni razem
+# sns.displot(df.query("Gender=='Male'").Weight)
+# sns.displot(df.query("Gender=='Female'").Weight)
+# plt.show()
+
+#gender, dana nienumeryczna
+df = pd.get_dummies(df)  #zamienia dane niemeryczne, na numeryczne
+del(df["Gender_Male"])   #usuń kolumnę
 print(df.head(5))
