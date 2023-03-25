8fe558fe57ebc9cbf2ffbd21061ee9cf8aacb161
musiakam <kamil.musial@tietoevry.com>
2023-03-25 09:36:08 +0100

    1

--- 1_regresja_liniowa.py
+++ 1_regresja_liniowa.py
@@ -1,3 +1,12 @@
 import pandas as pd    #'as'  alias
 from sklearn.linear_model import LinearRegression
 
+df = pd.read_csv('weight-height.csv')
+#print(type(df))
+#print(df)
+print(df.head(5))  #5 wierszy
+#print(df.Height) #dana kolumna
+print(df.Gender.value_counts())
+df.Height *= 2.54
+df.Weight /= 2.2
+print(df.head(5))
