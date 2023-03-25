af21fe9478623ca7212a08fdf671e5937cb85e62
musiakam <kamil.musial@tietoevry.com>
2023-03-25 10:45:45 +0100

    1

--- 1_regresja_liniowa.py
+++ 1_regresja_liniowa.py
@@ -21,4 +21,17 @@ print(df.head(5))
 #gender, dana nienumeryczna
 df = pd.get_dummies(df)  #zamienia dane niemeryczne, na numeryczne
 del(df["Gender_Male"])   #usuń kolumnę
+df.rename(columns={'Gender_Female': 'Gender'}, inplace=True)
 print(df.head(5))
+#dane na stole
+
+model = LinearRegression()   #wybieram algorytm
+model.fit(df[ ['Height', 'Gender'] ], df['Weight'] )   #policz
+print(model.coef_, model.intercept_)
+print('wzór: Height * ',model.coef_[0], '+ Gender * ',model.coef_[1],' = Weight')
+
+#Własna formuła
+gender = 0  #male
+height = 192
+weight = model.intercept_ + model.coef_[0] * height + model.coef_[1] * gender
+print(weight)
