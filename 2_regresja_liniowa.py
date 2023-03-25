87593f98a3a67720ee7a5600141f16da8cd0d872
musiakam <kamil.musial@tietoevry.com>
2023-03-25 12:44:54 +0100

    1

--- 2_regresja_liniowa.py
+++ 2_regresja_liniowa.py
@@ -25,14 +25,16 @@ q3 = df.describe().loc['75%','cena']
 print(_min, q1, q3)
 
 df1 = df[ (df.cena >= _min) & (df.cena <= q3) ]
-sns.displot(df1.cena)
-plt.show()
+# sns.displot(df1.cena)
+# plt.show()
 
 #podział na dane treningowe i testowe
 X = df1.iloc[:, 2: ]   #liczba_pieter  liczba_pokoi  pietro  powierzchnia  rok_budowy
 y = df1.cena
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+#wybór i trenowanie modelu
 model = LinearRegression()
 model.fit(X_train, y_train)
 print(model.score(X_test, y_test))
+print(model.coef_)
